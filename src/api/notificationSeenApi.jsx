67963a358348e561a48683dd23f56dedd9980cc8
Sreelakshmi2008 <sreetb2008@gmail.com>
2023-12-25 21:12:24 +0530

    notification

--- src/api/notificationSeenApi.jsx
+++ src/api/notificationSeenApi.jsx
@@ -1,27 +1,33 @@
-import axiosInstance from "../utils/axiosInstance";
-import { baseUrl } from "../utils/constants";
+import axios from 'axios';
 
-const getNotificationsApi = async () => {
-    try {
-        
-        const response = await axiosInstance.get(`${baseUrl}posts/notifications/`, {
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          }
-        });
-        if  (response.status === 200) {
-            return response.data;
-        } else {
-            console.log(response.error)
-        }
-        console.log(response.data);
-      } catch (error) {
-        console.error(error);
+import {  baseUrl } from '../utils/constants';
+import axiosInstance from '../utils/axiosInstance';
+
+const notificationSeenApi = async (notificationId) =>  {
+  try {
+    const accessToken = localStorage.getItem('access_token');
+
+    // Make sure to include the 'Content-Type' header and remove extra object nesting
+    const response = await axiosInstance.post(
+      `${baseUrl}/posts/notifications-seen/${notificationId}/`,
+      null, // No request body, you can use null
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json', // Include the Content-Type header
+        },
+      }
+    );
+
+    if (response.status === 200) {
+      return response.data;
+    } else {
+      console.error('Error:', response.data);
     }
-  };
+  } catch (error) {
+    console.error('Error:', error);
+    // Handle errors here
+  }
+};
 
-export default getNotificationsApi
-
-
-
+export default notificationSeenApi;
